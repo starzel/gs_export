41610d77054ecceee1095b6cbfea9d45a14353a0
Philip Bauer <bauer@starzel.de>
2012-11-11 14:53:39 +0100

    Make email-message more verbose

--- src/gs_export/__init__.py
+++ src/gs_export/__init__.py
@@ -27,16 +27,17 @@ class Export(object):
         try:
             return self.getters[key]('gs_export', key)
         except ConfigParser, e:
-            logger.error('You did not fully define your configuration (%s)'
-                         , str(e))
+            logger.error('You did not fully define your configuration (%s)',
+                         str(e))
             raise
 
     def sendmail(self, msg):
         c = self.getConfig
         with outbox.Outbox(username=c('mail_user'),
                            password=c('mail_password'),
-                           server=c('mail_server'), port=c('mail_port'
-                           ), mode=c('mail_mode')) as obox:
+                           server=c('mail_server'),
+                           port=c('mail_port'),
+                           mode=c('mail_mode')) as obox:
             obox.username = c('mail_from')
             obox.send(outbox.Email(subject='GS Profile needs action: %s/manage_main'
                        % c('base_url'), body=msg,
@@ -45,15 +46,15 @@ class Export(object):
     def __call__(self):
         c = self.getConfig
         urlob = urllib.urlopen(c('base_url'),
-                               urllib.urlencode({'manage_exportAllSteps:method': ' Export all steps '
-                               , '__ac_name': c('user'),
-                               '__ac_password': c('password')}))
+                               urllib.urlencode({
+                                  'manage_exportAllSteps:method':' Export all steps ',
+                                  '__ac_name': c('user'),
+                                  '__ac_password': c('password')}))
         try:
             tarob = tarfile.open(fileobj=StringIO(urlob.read()))
         except tarfile.ReadError, e:
             self.sendmail(str(e)
-                          + ' Maybe the server is down, or the credentials are wrong'
-                          )
+                    + ' Maybe the server is down, or the credentials are wrong')
             sys.exit(1)
 
         path = c('path')
@@ -69,8 +70,12 @@ class Export(object):
                     shutil.rmtree(ignore)
             if create_git_repo:
                 subprocess.call(['git', 'init'])
-            result = subprocess.check_output(['git', 'status', '-s'])
-            if result:
+            result_status = subprocess.check_output(['git', 'status', '-s'])
+            result_diff = subprocess.check_output(['git', 'diff'])
+            result = "The generic-setup-export is stored in '" + path + \
+                "'\r\n\r\n" + "Files with changes: \r\n" + result_status + \
+                "\r\n\r\nDetails: \r\n" + result_diff
+            if result_status:
                 self.sendmail(result)
         finally:
             os.chdir(curdir)
@@ -81,7 +86,6 @@ def main():
     try:
         config.read(sys.argv[1])
     except Exception, e:
-        logger.error('You must provide an absolute path to a configuration file! (%s)'
-                     , str(e))
+        logger.error('You must provide an absolute path to a configuration file! (%s)', str(e))
         raise
     Export(config)()
